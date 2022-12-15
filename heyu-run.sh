36fbed11b7086a8112bd6740377c445793277281
roblowe8 <120499832+roblowe8@users.noreply.github.com>
2022-12-15 03:50:28 -0700

    Final

--- heyu-run.sh
+++ heyu-run.sh
@@ -2,14 +2,16 @@
 
 if [ ! -f /etc/heyu/x10config ]; then
   cp /etc/heyu.default/* /etc/heyu
-  echo "x10config.sample and x10.sched.sample have been copied to your config directory."  #1>&2
-  echo "Please use them to create x10.conf and x10.sched and re-run." #1>&2
+  echo "x10config.sample and x10.sched.sample have been copied to your config directory."  1>&2
+  echo "Please use them to create x10.conf and x10.sched and re-run." 1>&2
   exit;
 fi
 
-heyu stop #1>&2
-heyu engine #1>&2
-heyu start #1>&2
+cd /etc/heyu
+
+heyu stop 1>&2
+heyu engine 1>&2
+heyu start 1>&2
 # heyu upload 1>&2
 # heyu setclock 1>&2
 
@@ -19,5 +21,5 @@ else
     export prefix="/$URL_KEY/"
 fi
 
-echo "ready to run perl script." #1>&2
+#echo "ready to run perl script." #1>&2
 perl /etc/heyu/heyu-mqtt.pl
